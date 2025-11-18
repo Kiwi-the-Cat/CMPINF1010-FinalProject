87ebcdac7b7550ae043527eee717399253a1d711
Kiwi-the-Cat <remiozzy@gmail.com>
2025-11-17 22:41:22 -0500

    switching datasets

---
+++ watersheds.py
@@ -0,0 +1,16 @@
+""" 
+Dataset Info:
+Title: Pittsburgh Watersheds
+Source: https://catalog.data.gov/dataset/pittsburgh-watersheds
+Description: Watersheds for surface water streams and rivers within Pittsburgh boundaries. 
+    StreamStats was the original source, and the geometry was simplified afterwards.
+Files:
+- d6ba2cc7-af46-41ab-9a2e-f8711bfeeabe.csv
+- pittsburghpapgh-watersheds.geojson
+- CityBoundary_-7448898963963379882.geojson
+"""
+
+# Author: Max
+# Purpose: This script parses and processes the Pittsburgh Watersheds dataset.
+
+# Import necessary libraries
--- watershedset.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-dataset = pd.read_csv('d6ba2cc7-af46-41ab-9a2e-f8711bfeeabe.csv')
-print(dataset.head())
